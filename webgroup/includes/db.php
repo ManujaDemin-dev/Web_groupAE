ae7f2d7bfe32ceb926985b00656cb3869e718093
Manuja Demin <156160654+Manuja-D@users.noreply.github.com>
2024-12-16 04:59:11 +0530

    about us html

--- webgroup/includes/db.php
+++
@@ -1,8 +0,0 @@
-<?php
-try {
-    $pdo = new PDO('mysql:host=localhost;dbname=student_community_app', 'root', '');
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    die('Database connection is failed: ' . $e->getMessage());
-}
-?>
