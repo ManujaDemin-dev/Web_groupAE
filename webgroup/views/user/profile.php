c5b1f11f34e9a56447d4eabc8c4b8c8fedb6d40b
kpabasara12003 <kakpjayawickrama@students.nsbm.ac.lk>
2024-12-19 18:23:33 +0530

    6.23
    sorry

--- webgroup/views/user/profile.php
+++ webgroup/views/user/profile.php
@@ -135,7 +135,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
         <div class="form-group">
             <label for="description">Description</label>
-            <textarea id="description" name="description"><?= htmlspecialchars($user['description'] ?? '') ?></textarea>
+            <textarea id="description" name="description" maxlength="300"><?= htmlspecialchars($user['description'] ?? '') ?></textarea>
         </div>
 
         <button type="submit" id="buttonon">Update Profile</button>
