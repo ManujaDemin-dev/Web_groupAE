f78d43369fea90c661043c66a6313d4c2db6153b
kpabasara12003 <kakpjayawickrama@students.nsbm.ac.lk>
2024-12-24 12:29:43 +0530

    12.26
    ok

--- webgroup/aboutus.php
+++ webgroup/aboutus.php
@@ -257,7 +257,7 @@
 </head>
 <body>
     <?php
-        include("header.html");
+        include("header1.html");
     ?>
     <!-- Intro Section -->
     <div class="intro">
@@ -327,6 +327,6 @@
 </body>
 </html>
 <?php
-    include("footer.html");
+    include("footer1.html");
     
 ?>
