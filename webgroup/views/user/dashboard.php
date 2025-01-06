41afdaee6d2e0b4b673130ace4f1b7fac40a0a1e
kpabasara12003 <kakpjayawickrama@students.nsbm.ac.lk>
2025-01-06 21:14:03 +0530

    music
    test

--- webgroup/views/user/dashboard.php
+++ webgroup/views/user/dashboard.php
@@ -276,7 +276,7 @@ h1 {
 
 <div class="row">
     <button class="feature"><a href="./../../../Pomodoro Timer/pomodoro.html" target="_blank"> Pomodoro timer </a></button>
-    <button class="feature"><a href="#">Listen to music </a></button>
+    <button class="feature"><a href="./music.php" target="_blank">Listen to music </a></button>
 <?php   $role = $_SESSION['role'];
 if ($role == 'admin') {
    
