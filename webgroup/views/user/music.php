df2258897a9325f94b373e4be6f1dcb61f7c0133
Manuja Demin <mmdwijesinghe@students.nsbm.ac.lk>
2025-01-24 07:33:24 +0530

    Music.php update

--- webgroup/views/user/music.php
+++ webgroup/views/user/music.php
@@ -5,38 +5,82 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>music</title>
+    <style>
+      @import url('https://fonts.googleapis.com/css2?family=Sour+Gummy:ital,wght@0,100..900;1,100..900&display=swap');
+
+
+      .con{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        background-color: blanchedalmond;
+        min-height: 100vh;
+      }
+
+      .midcon{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        /* background-color: gray; */
+        width: 1000px;
+        height: 600px;
+      }
+
+      h1{
+        font-family: "Sour Gummy", serif;
+        font-optical-sizing: auto;
+        font-weight: 900;
+        font-style: normal;
+        font-size: 3rem;
+        color: #0D3B66;
+      }
+
+      .ptitle{
+        font-size: 1.0rem;
+        color:rgb(154, 154, 154);
+      }
+
+      @media (max-width: 768px) {
+
+        .midcon{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+        /* background-color: gray; */
+        width: 1000px;
+        height: 600px;
+      }
+
+        h1{
+        font-family: "Sour Gummy", serif;
+        font-optical-sizing: auto;
+        font-weight: 900;
+        font-style: normal;
+        font-size: 1.5rem;
+        color: #0D3B66;
+      }
+
+      iframe{
+        height: 375px;
+      }
+
+      .ptitle{
+        font-size: 0.5rem;
+        color:rgb(154, 154, 154);
+      }
+
+
+      }
+    </style>
 </head>
 <body>
+  <div class="con">
+    <div class="midcon">
+      <h1>LOFI BEATS FOR STUDYING🎧<p class="ptitle">Carefully crafted by groupAE</p></h1>
+      <iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/3bevTxSrGdh3qShUcODtxE?utm_source=generator" width="100%" height="600" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
 
-<iframe 
-  width="50%" 
-  height="300" 
-  src="https://www.youtube.com/embed?list=OLAK5uy_khq09L7IBJO_IudA9F2JVgJapGPG3r23o" 
-  frameborder="0" 
-  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
-  allowfullscreen>
-</iframe>
-
-
-
-<iframe 
-  width="50%" 
-  height="300" 
-  src="https://www.youtube.com/embed?list=OLAK5uy_kggaeBhx4sbBEZuiQiF8xef6_uI6G1o6I" 
-  frameborder="0" 
-  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
-  allowfullscreen>
-</iframe>
-    
-
-<iframe 
-  width="50%" 
-  height="300" 
-  src="https://www.youtube.com/embed?list=OLAK5uy_kggaeBhx4sbBEZuiQiF8xef6_uI6G1o6I" 
-  frameborder="0" 
-  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
-  allowfullscreen>
-</iframe>
-    
+    </div>
+  </div>
 </body>
 </html>
