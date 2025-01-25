b56614cf5b92f999526664a73c8a316f1dc70573
kpabasara12003 <kakpjayawickrama@students.nsbm.ac.lk>
2025-01-25 14:19:41 +0530

    dab php
    db

--- webgroup/includes/db.php
+++ webgroup/includes/db.php
@@ -1,10 +1,13 @@
 <?php
 try {
-    $pdo = new PDO('mysql:host=bhwjsuparexjfjhzlg0z-mysql.services.clever-cloud.com;dbname=bhwjsuparexjfjhzlg0z', 'ucq7kw8qyrt5l7x1', 'qyhSw757dUINoaxn8pgN');
+    //$pdo = new PDO('mysql:host=bhwjsuparexjfjhzlg0z-mysql.services.clever-cloud.com;dbname=bhwjsuparexjfjhzlg0z', 'ucq7kw8qyrt5l7x1', 'qyhSw757dUINoaxn8pgN');
 
-    //$pdo = new PDO('mysql:host=localhost;dbname=student_community_app', 'root', '');
+    $pdo = new PDO('mysql:host=157.245.99.237;dbname=focusnet@ae', 'groupAE', 'php@$aelinx_');
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die('Database connection is failed: internet oni 😁😎' . $e->getMessage());
 }
 ?>
+
+
+
